a4c867bc74bed8204edaa610f4966baaa9346346
Jayden Tiew <106733343+Jayden185YT@users.noreply.github.com>
2024-03-02 09:39:28 +0000

    Submission

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 num = input('Enter a number (decimal only): ')
 # type your code here
-
+decimal = str(num)
+decimal = decimal.split(".")
+dp = len(decimal[1])
 
 
 # do not change any code beyond this point
